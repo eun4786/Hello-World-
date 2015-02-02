3c025f094f913ff2a069c5800d9a0a9f6615cc53
Yukariko <rkdals7979@naver.com>
2015-02-02 15:06:15 +0900

    sort

--- temp123/소스.cpp
+++ temp123/소스.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,6 +9,17 @@ int main()
 	cout<<"qwerty\n";
 	cout<<"Hello, world!"<<endl;
 	cout<<"a"<<endl;
+
+	int a[10]={1,6,3,21,7,5,2,6,73};
+
+	for(int i=0; i<10; i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+	sort(a, a+10);
+
+	for(int i=0; i<10; i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
 
 	return 0;
 }
